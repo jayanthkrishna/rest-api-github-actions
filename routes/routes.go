9feb9b4e5d5131deabbe35a50b96e517f0c1c977
jayanthkrishna <jayanth.krishna.g@gmail.com>
2023-09-11 23:27:11 +0530

    Added Api endpoints

--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,8 @@ func GetRoutes(app *fiber.App) {
 
 	app.Get("/", contollers.HelloWorld)
 
+	app.Get("/api/v1/book", contollers.GetBooks)
+	app.Get("/api/v1/book/:id", contollers.GetBook)
+	app.Post("/api/v1/book", contollers.NewBook)
+	app.Delete("/api/v1/book/:id", contollers.DeleteBook)
 }
